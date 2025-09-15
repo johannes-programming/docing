f6ca2a73f707e82ed358d553ae3aa74a72cb254a
Johannes <johannes.programming@gmail.com>
2025-09-16 00:04:31 +0200

    Version 0.1.1

--- src/docing/core/__init__.py
+++ src/docing/core/__init__.py
@@ -1,45 +1,51 @@
+import enum
+import functools
+import tomllib
+from importlib import resources
 from typing import *
 
 __all__ = ["easy"]
 
-_DICT = {
-    "__add__": "This magic method implements self+other.",
-    "__and__": "This magic method implements self&other.",
-    "__call__": "This magic method implements calling self.",
-    "__divmod__": "This magic method implements divmod(self, other).",
-    "__floordiv__": "This magic method implements self//other.",
-    "__ge__": "This magic method implements self>=other.",
-    "__gt__": "This magic method implements self>other.",
-    "__init__": "This magic method initializes self.",
-    "__le__": "This magic method implements self<=other.",
-    "__lshift__": "This magic method implements self<<other.",
-    "__lt__": "This magic method implements self<other.",
-    "__matmul__": "This magic method implements self@other.",
-    "__mod__": "This magic method implements self%other.",
-    "__mul__": "This magic method implements self*other.",
-    "__or__": "This magic method implements self|other.",
-    "__pow__": "This magic method implements pow(self, other).",
-    "__radd__": "This magic method implements other+self.",
-    "__rand__": "This magic method implements other&self.",
-    "__rdivmod__": "This magic method implements divmod(other, self).",
-    "__rfloordiv__": "This magic method implements other//self.",
-    "__rlshift__": "This magic method implements other<<self.",
-    "__rmatmul__": "This magic method implements other@self.",
-    "__rmod__": "This magic method implements other%self.",
-    "__rmul__": "This magic method implements other*self.",
-    "__ror__": "This magic method implements other|self.",
-    "__rpow__": "This magic method implements pow(other, self).",
-    "__rrshift__": "This magic method implements other>>self.",
-    "__rshift__": "This magic method implements self>>other.",
-    "__rsub__": "This magic method implements other-self.",
-    "__rtruediv__": "This magic method implements other/self.",
-    "__rxor__": "This magic method implements other^self.",
-    "__sub__": "This magic method implements self-other.",
-    "__truediv__": "This magic method implements self/other.",
-    "__xor__": "This magic method implements self^other.",
-}
+
+class Util(enum.Enum):
+    util = None
+
+    @functools.cached_property
+    def data(self: Self) -> dict:
+        text: str = resources.read_text("docing.core", "cfg.toml")
+        data: dict = tomllib.loads(text)
+        return data
+
+    @functools.cached_property
+    def lookup(self: Self) -> dict[str, str]:
+        ans: dict = dict()
+        a: str
+        b: str
+        x: str
+        y: str
+        for a, b in self.magicmethodsdict.items():
+            x = "__%s__" % a
+            y = "This magic method %s." % b
+            ans[x] = y
+        return ans
+
+    @functools.cached_property
+    def magicmethodsdict(self: Self) -> dict[str, str]:
+        ans: dict = dict()
+        a: str
+        b: str
+        c: str
+        d: str
+        for a, b in self.data["classical-patterns-by-name-prefix"].items():
+            for c, d in self.data["classical-operators-by-name"].items():
+                ans[a + c] = "implements %s" % (b % d)
+        for a, b in self.data["atypical-magic-implements-by-name"].items():
+            ans[a] = "implements %s" % b
+        for a, b in self.data["atypical-magic-explanations-by-name"].items():
+            ans[a] = b
+        return ans
 
 
 def easy(target: Any) -> Any:
-    target.__doc__ = _DICT[target.__name__]
+    target.__doc__ = Util.util.lookup[str(target.__name__)]
     return target
